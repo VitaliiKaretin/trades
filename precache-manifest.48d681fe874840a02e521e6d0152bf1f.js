afeb53267188681cca03881b7882bf4728b215b9
forcef12 <forcef12@gmail.com>
2020-02-21 18:57:30 +0200

    Updates

---
+++ precache-manifest.48d681fe874840a02e521e6d0152bf1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849ed364f039fa6a15cb52f32ac12bbf",
+    "url": "/ForceF1/trades/index.html"
+  },
+  {
+    "revision": "702bf92be21b0f8c9034",
+    "url": "/ForceF1/trades/static/css/main.f4bae363.chunk.css"
+  },
+  {
+    "revision": "4e0521c0afe02b9b98e8",
+    "url": "/ForceF1/trades/static/js/2.b02ed86e.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/ForceF1/trades/static/js/2.b02ed86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702bf92be21b0f8c9034",
+    "url": "/ForceF1/trades/static/js/main.543ca2dd.chunk.js"
+  },
+  {
+    "revision": "edafd07f1dc4650c8521",
+    "url": "/ForceF1/trades/static/js/runtime-main.381902aa.js"
+  },
+  {
+    "revision": "e2f499bff32f178808177cf549c7fb6c",
+    "url": "/ForceF1/trades/static/media/eth.e2f499bf.png"
+  }
+]);
